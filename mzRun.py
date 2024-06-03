fb7491d371ea1efe310f11596fcb9fd9d6821984
Jayana N <84286214+jayana-cpc@users.noreply.github.com>
2024-06-03 10:19:33 -0700

    Add files via upload
    Added 2 separate cost models. 
    Added to-do list.
    Edits to python script.

--- mzRun.py
+++ mzRun.py
@@ -10,24 +10,32 @@ def splitter(result):
     cost = int(costPart)
     return cost, equation
 
-model = minizinc.Model("Playground_py.mzn")
+model = minizinc.Model("costModel1.mzn")
+model2 = minizinc.Model("costModel2.mzn")
 chuffed = minizinc.Solver.lookup("chuffed")
-Cs = list(range(1, 10))
+Cs = list(range(1, 101))
 results = []
 
 for C in Cs:
     instance = minizinc.Instance(chuffed, model)
+    instance2 = minizinc.Instance(chuffed, model2)
+
     instance["C"] = C
+    instance2["C"] = C
+
     start = time.time() 
     result = instance.solve()
+    result2 = instance2.solve()
     end = time.time() 
     if result:
         cost, equation = splitter(result)
-        results.append({"C": C, "Bits": C.bit_length(), "Cost": cost, "Equation": equation, "Runtime": end - start})
+        cost2, equation2 = splitter(result2)
+
+        results.append({"C": C, "Bits": C.bit_count(), "Cost (k+1)": cost, "Cost": cost2,"Equation": equation, "Runtime": end - start})
         print(C, " tested")
 
 with open("results.csv", "w", newline="") as csvfile:
-    fieldnames = ["C", "Bits", "Equation", "Cost", "Runtime"]
+    fieldnames = ["C", "Bits", "Equation", "Cost (k+1)", "Cost", "Runtime"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerows(results)
