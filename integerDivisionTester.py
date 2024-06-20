1a3afcaf2c5bc72fe71ac8d9e45fe0df1b5c1dfe
Jayana N <84286214+jayana-cpc@users.noreply.github.com>
2024-06-20 12:30:47 -0700

    Updated Integer Division Tester

--- integerDivisionTester.py
+++ integerDivisionTester.py
@@ -1,36 +1,71 @@
+"""
+This program tests the accuracy of a Minizinc replication of integer division for a round to zero scheme 
+defined in a paper by Drane and Cheung (https://cas.ee.ic.ac.uk/people/gac1/pubs/TheoISCAS12.pdf).
+
+
+The workflow is as follows:
+1. Initialize parameters `d` and `n`. `d` being the divisor. `n` being the bit length.
+2. Define a `test_accuracy` function to test the accuracy of the solution by comparing the left-hand side (lhs) and right-hand side (rhs) values for all possible `x` values.
+3. Solve Minizinc model.
+4. Test the accuracy of the model and save the comparison results to a CSV file.
+5. Print a message indicating whether the model's accuracy is satisfactory.
+
+"""
+
 import minizinc
-import math
+import numpy as np
+import pandas as pd 
 
-d = 7
-n = 6
+d = np.uint8(7)
+n = np.uint8(6)
 
-def minh(a, b):
-    p = max(a.bit_length(), b.bit_length())  
-    c = 0
-    for i in range(p - 1, -1, -1):  
-        bit_a = (a >> i) & 1  
-        bit_b = (b >> i) & 1  
-        if bit_a == bit_b:
-            c |= (bit_a << i) 
-            a &= ~(1 << i) 
-        else:
-            c += 2**(i + 1)  
-            break
-    return c
+def splitter(input_string):
+    numbers = input_string.split()
+    
+    a_BF = np.uint8(int(numbers[0]))
+    b_BF = np.uint8(int(numbers[1]))
+    k_BF = np.uint8(int(numbers[2]))
+    
+    return a_BF, b_BF, k_BF
 
-model = minizinc.Model("findK.mzn")
-coinbc = minizinc.Solver.lookup("coinbc")
-findK = minizinc.Instance(coinbc, model)
-findK["d"] = d
-findK["n"] = n
-result = findK.solve()
-k = int(str(result))
-a = math.floor(pow(2, k) // d)
+def test_accuracy(a_BF, b_BF, k_BF, d, n):
+    max_val = (1 << n) - 1
+    x_vals = [i for i in range(max_val + 1)]
+    
+    lhs = []
+    rhs = []
+    
+    for x in x_vals:
+        lhs_value = x // d
+        rhs_value = ((a_BF * x) + b_BF) // (1 << k_BF)
+        
+        lhs.append(lhs_value)
+        rhs.append(rhs_value)
+    
+    accuracy = all(l == r for l, r in zip(lhs, rhs))
+    
+    df = pd.DataFrame({
+        'x_vals': x_vals,
+        'lhs': lhs,
+        'rhs': rhs
+    })
+    
+    df.to_csv('lhs_rhs_values.csv', index=False)
+    
+    return accuracy
 
-if a*d - pow(2, k) > 0:
-    b = 0
+model = minizinc.Model("integerDivision.mzn")
+solver = minizinc.Solver.lookup("highs")
+instance = minizinc.Instance(solver, model)
+instance["d"] = d
+instance["n"] = n
+result = instance.solve()
+a_BF, b_BF, k_BF = splitter(str(result))
+
+print("Start Accuracy testing")
+
+result = test_accuracy(a_BF, b_BF, k_BF, d, n)
+if result:
+    print("The test accuracy is satisfactory. Model was accurate.")
 else:
-    b = minh(((pow(2, k) - (a * d)) * math.floor(pow(2, n) // d)), (pow(2, k) - ((a * (d - 1)) - 1)))
-
-print(f"a = {a}, b = {b}, k = {k}")
-
+    print("Model was inaccurate")
